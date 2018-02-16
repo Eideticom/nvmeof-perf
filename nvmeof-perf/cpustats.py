912eeb6392451f1882c41b1f1c0d333b733ba759
Logan Gunthorpe <logang@deltatee.com>
2018-02-16 14:52:14 -0700

    Added cpustats timeline

--- nvmeof-perf/cpustats.py
+++ nvmeof-perf/cpustats.py
@@ -14,7 +14,27 @@
 ##
 ########################################################################
 
+import utils
+from suffix import Suffix
+
 import os
+
+from collections import namedtuple
+
+class CpuStats(namedtuple("CpuStats", ["user", "system", "idle",
+                                       "iowait", "total", "intr",
+                                       "mem_total", "mem_avail",
+                                       "mem_used"])):
+    def __sub__(a, b):
+        return CpuStats(user=a.user - b.user,
+                        system=a.system - b.system,
+                        idle=a.idle - b.idle,
+                        iowait=a.iowait - b.iowait,
+                        total=a.total - b.total,
+                        intr=a.intr - b.intr,
+                        mem_total=a.mem_total,
+                        mem_avail=a.mem_avail,
+                        mem_used=a.mem_used)
 
 def cpu_stats():
     ret = {}
@@ -27,9 +47,13 @@ def cpu_stats():
 
         if data[0] == "cpu":
             ret["user"] = (int(data[1]) + int(data[2])) * time_per_jiffie
-            ret["system"] = int(data[3]) * time_per_jiffie
+            ret["system"] = ((int(data[3]) + int(data[6]) + int(data[7])) *
+                              time_per_jiffie)
             ret["idle"] = int(data[4]) * time_per_jiffie
-            ret["iowait"] = int(data[4]) * time_per_jiffie
+            ret["iowait"] = int(data[5]) * time_per_jiffie
+            ret["total"] = (ret["user"] + ret["system"] + ret["idle"] +
+                            ret["iowait"])
+
         elif data[0] == "intr":
             ret["intr"] = int(data[1])
 
@@ -50,7 +74,55 @@ def cpu_stats():
 
     ret["mem_used"] = ret["mem_total"] - ret["mem_avail"]
 
-    return ret
+    return CpuStats(**ret)
+
+class CpuTimeline(utils.Timeline):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.last = None
+
+    def next(self):
+        super().next()
+
+        stats_new = cpu_stats()
+
+        if self.last:
+            stats = stats_new - self.last
+        else:
+            stats = stats_new
+
+        self.last = stats_new
+
+        return stats
+
+    def print_next(self, indent=""):
+        stats = self.next()
+
+        print("{}CPU Stats:".format(indent))
+        indent += "  "
+
+        def print_line(typ):
+            print("{}{:<39} {:>7.1f}  \t{:>7.1%}".
+                  format(indent, typ.title() + ":", getattr(stats, typ),
+                         getattr(stats, typ) / stats.total))
+
+        print_line("idle")
+        print_line("user")
+        print_line("system")
+        print_line("iowait")
+
+        mem_used = Suffix(stats.mem_used)
+
+        print("{}{:<39} {:>7.1f}  \t{:>7.1%}".
+              format(indent, "Memory Used:", mem_used,
+                     stats.mem_used / stats.mem_total))
 
 if __name__ == "__main__":
-    print(cpu_stats())
+    import time
+
+    tl = CpuTimeline()
+    while True:
+        print(time.asctime())
+        tl.print_next();
+        print()
+        print()
