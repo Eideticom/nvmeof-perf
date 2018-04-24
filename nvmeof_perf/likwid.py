952922568dc26983504111361cae10d3db46f15c
Logan Gunthorpe <logang@deltatee.com>
2018-04-24 10:21:40 -0600

    Add support for multiple groups to likwid to print L3 cache stats

--- nvmeof_perf/likwid.py
+++ nvmeof_perf/likwid.py
@@ -32,9 +32,10 @@ class LikwidPerfMixin(object):
                            r"( \[(?P<units>.*?)\])?\s+" +
                            r"\|\s+(?P<value>[0-9\.e\+]+)\s+\|")
 
-    def __init__(self, group="MEM", cpu="S0:0", *args, **kwargs):
+    def __init__(self, groups=["MEM"], cpu="S0:0", *args, **kwargs):
         super(LikwidPerfMixin, self).__init__(*args, **kwargs)
-        self.exe = ["likwid-perfctr", "-f", "-g", group, "-C", cpu] + self.exe
+        groups = [y for x in zip(["-g"] * len(groups), groups) for y in x]
+        self.exe = ["likwid-perfctr", "-f", "-C", cpu] + groups + self.exe
         self.likwid_stats = {}
         self.likwid_units = {}
 
@@ -49,29 +50,20 @@ class LikwidPerfMixin(object):
 class LikwidException(Exception):
     pass
 
-class LikwidTimeline(proc.ProcRunner):
-    exe = ["likwid-perfctr"]
+class _LikwidGroup(object):
     col_re = re.compile(r"^(?P<name>[^\[\|]+)(\[(?P<units>.*?)\])?")
-    kill_me = True
 
-    def __init__(self, group="MEM", cpu=None, period=1.0, **kwargs):
-        super().__init__(self, **kwargs)
-
-        if cpu == None:
-            cpu = likwid_all_sockets()
-
-        self.args = ["-f", "-g", group, "-c", cpu, "-O"]
-        data = sp.check_output(self.exe + self.args + ["-S", "10ms"])
-        self.args += ["-t", "{}s".format(period)]
-        cdata = csv.reader(StringIO(data.decode()), delimiter=",")
+    def __init__(self, name, group_id, cdata):
+        self.cols = []
+        self.units = []
+        self.name = name
+        self.group_id = group_id
 
         for row in cdata:
-            if row[0] == "TABLE" and row[1] == "Group 1 Metric":
+            if row[0] == "TABLE" and row[1] == "Group {} Metric".format(group_id):
                 break
 
         self.cpus = [c for c in next(cdata)[1:] if c]
-        self.cols = []
-        self.units = []
 
         for row in cdata:
             if row[0] == "TABLE":
@@ -102,10 +94,35 @@ class LikwidTimeline(proc.ProcRunner):
                 self.multiplier[i] = 1 << 40
                 self.units[i] = "B"
 
+
+class LikwidTimeline(proc.ProcRunner):
+    exe = ["likwid-perfctr"]
+    kill_me = True
+
+    def __init__(self, groups=["L3", "MEM"], cpu=None, period=1.0, **kwargs):
+        super().__init__(self, **kwargs)
+
+        groups_args = [y for x in zip(["-g"] * len(groups), groups) for y in x]
+
+        if cpu == None:
+            cpu = likwid_all_sockets()
+
+        period /= len(groups)
+
+        self.args = ["-f", "-c", cpu, "-O"] + groups_args
+        data = sp.check_output(self.exe + self.args + ["-S", "10ms"])
+        self.args += ["-t", "{}s".format(period)]
+        cdata = csv.reader(StringIO(data.decode()), delimiter=",")
+
+        self.groups = OrderedDict()
+        for i, name in enumerate(groups):
+            grp_id = i + 1
+            self.groups[grp_id] = _LikwidGroup(name, grp_id, cdata)
+
         self.queue = Queue()
 
     def wait_until_ready(self):
-        while self.queue.empty():
+        while self.queue.qsize() < len(self.groups):
             time.sleep(0.1)
 
     def __enter__(self):
@@ -114,42 +131,46 @@ class LikwidTimeline(proc.ProcRunner):
         return ret
 
     def process_line(self, line):
-        if line.startswith("1 "):
-            cols = line.split()
-        elif line.startswith("1,"):
+        if not line[0].isdigit():
+            return
+
+        if "," in line:
             cols = line.split(",")
         else:
-            return
+            cols = line.split()
 
         group_id = int(cols.pop(0))
         events = int(cols.pop(0))
         nthreads = int(cols.pop(0))
         timestamp = float(cols.pop(0))
 
-        if events != len(self.cols):
+        grp = self.groups[group_id]
+
+        if events != len(grp.cols):
             raise LikwidException("Unexpected number of events: {} != {}",
-                                  events, len(self.cols))
+                                  events, len(grp.cols))
 
         cpus = []
         for i in range(len(cols) // events):
             ncols = cols[i::(len(cols) // events)]
-            ncols = [float(x) * m for x, m in zip(ncols, self.multiplier)]
-            cpus.append(OrderedDict(zip(self.cols, ncols)))
+            ncols = [float(x) * m for x, m in zip(ncols, grp.multiplier)]
+            cpus.append(OrderedDict(zip(grp.cols, ncols)))
 
-        self.queue.put(cpus)
+        self.queue.put((cpus, grp))
 
     def next(self):
-        self.latest = self.queue.get()
+        self.latest = OrderedDict()
+        for g in self.groups.values():
+            cpus, grp = self.queue.get()
+            self.latest[grp.name] = cpus, grp
         return self.latest
 
-    def print_next(self, indent=""):
-        stats = self.next()
-
+    def print_MEM(self, grp, stats, indent=""):
         print("{}{c.bold}Memory Bandwidth Stats:{c.rst}".
               format(indent, c=colours))
         indent += "  "
 
-        for cname, c in zip(self.cpus, stats):
+        for cname, c in zip(grp.cpus, stats):
             read = Suffix(c["Memory read data volume"])
             write = Suffix(c["Memory write data volume"])
             read_bw = Suffix(c["Memory read bandwidth"], unit="B/s")
@@ -160,12 +181,39 @@ class LikwidTimeline(proc.ProcRunner):
             print("{}{:<30} write: {:>7.1f}  \t{:>7.1f}".
                   format(indent, "", write, write_bw))
 
+    def print_L3(self, grp, stats, indent=""):
+        print("{}{c.bold}L3 Cache Stats:{c.rst}".
+              format(indent, c=colours))
+        indent += "  "
+
+        for cname, c in zip(grp.cpus, stats):
+            load = Suffix(c["L3 load data volume"])
+            evict = Suffix(c["L3 evict data volume"])
+            total = Suffix(c["L3 data volume"])
+
+            load_bw = Suffix(c["L3 load bandwidth"], unit="B/s")
+            evict_bw = Suffix(c["L3 evict bandwidth"], unit="B/s")
+            total_bw = Suffix(c["L3 bandwidth"], unit="B/s")
+
+            print("{}{:<30} load:  {:>7.1f}  \t{:>7.1f}".
+                  format(indent, cname, load, load_bw))
+            print("{}{:<30} evict: {:>7.1f}  \t{:>7.1f}".
+                  format(indent, "", evict, evict_bw))
+            print("{}{:<30} total: {:>7.1f}  \t{:>7.1f}".
+                  format(indent, "", total, total_bw))
+
+    def print_next(self, indent=""):
+        for name, (stats, grp) in self.next().items():
+            getattr(self, "print_" + name)(grp, stats, indent)
+
     def csv(self):
-        return tuple(x for y in self.latest for x in y.values())
+        return tuple(x for g in self.groups.values()
+                     for y in self.latest[g.name][0]
+                     for x in y.values())
 
     def csv_titles(self):
-        return tuple("{}:{}".format(c, t) for c in self.cpus
-                     for t in self.latest[0].keys())
+        return tuple("{}:{}".format(c, t) for g in self.groups.values()
+                     for c in g.cpus for t in self.latest[g.name][0][0].keys())
 
 def likwid_all_sockets():
     data = sp.check_output(["likwid-pin", "-p"]).decode()
