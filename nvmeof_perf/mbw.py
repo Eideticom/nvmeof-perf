207e8c64bb9c03b9a9867134eea23724c811b2f3
Logan Gunthorpe <logang@deltatee.com>
2018-07-16 17:52:11 -0600

    Add MBW timeline for a background process memory bandwith

--- nvmeof_perf/mbw.py
+++ nvmeof_perf/mbw.py
@@ -15,7 +15,8 @@
 ##
 ########################################################################
 
-from . import proc, likwid
+from . import proc, likwid, utils, colours
+from .suffix import Suffix
 
 import re
 from itertools import repeat, chain
@@ -52,6 +53,7 @@ class MBWRunner(proc.ProcRunner):
 
     def clear(self):
         self.rates = []
+        self.volume = 0
 
     def stats(self):
         r = self.rates[1:-1]
@@ -65,3 +67,51 @@ class MBWRunner(proc.ProcRunner):
 
 class LikwidMBWRunner(likwid.LikwidPerfMixin, MBWRunner):
     pass
+
+class MBWTimeline(utils.Timeline):
+    def __init__(self, devices=[], *args, **kwargs):
+        array_size_mb = kwargs.pop("array_size_mb", 512)
+        super().__init__(*args, **kwargs)
+
+        self.inst = MBWRunner(array_size_mb=array_size_mb,
+                              loops=100000000)
+
+    def __enter__(self):
+        self.inst.start()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.inst.__exit__(type, value, traceback)
+
+    def next(self):
+        super().next()
+
+        stats  = self.inst.stats()
+        self.inst.clear()
+
+        if stats:
+            self.latest = (stats["avg"], stats["volume"])
+        else:
+            self.latest = (0, 0)
+
+        return self.latest
+
+    def print_next(self, indent=""):
+        rate, volume = self.next()
+
+        print("{}{c.bold}Background MBW Stats:{c.rst}".format(indent, c=colours))
+        indent += "  "
+
+        rate = Suffix(rate, unit="B/s")
+        volume = Suffix(volume)
+
+        print("{}{:<38}{:>7.1f}".
+              format(indent, "copy rate:", rate))
+        print("{}{:<38}{:>7.1f}".
+              format(indent, "volume:", volume))
+
+    def csv(self):
+        return self.latest
+
+    def csv_titles(self):
+        return ["copy_rate", "volume"]
